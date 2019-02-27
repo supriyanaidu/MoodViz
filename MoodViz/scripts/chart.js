ea651f17ba7c04c3a3286f0fb7971835e68d8a0a
supriyanaidu <32039542+supriyanaidu@users.noreply.github.com>
2019-02-26 18:52:16 -0700

    Fixed Clear, Line and Bar chart buttons

--- MoodViz/scripts/chart.js
+++ MoodViz/scripts/chart.js
@@ -1,7 +1,17 @@
+$("#clearMood").click(function() {
+	$("#mondayMood").val("");
+	$("#tuesdayMood").val("");
+	$("#wednesdayMood").val("");
+	$("#thursdayMood").val("");
+	$("#fridayMood").val("");
+	$("#saturdayMood").val("");
+	$("#sundayMood").val("");
+})
+
 var moodData= [];
 var chartContext;
 
-function loadData(){
+function loadData(typeChart){
 	moodData=[];
 	var value = document.getElementById('mondayMood').value;
 	moodData.push(value);
@@ -17,43 +27,24 @@ function loadData(){
 	moodData.push(value);
 	value = document.getElementById('sundayMood').value;
 	moodData.push(value);
-	// var lineButtonClick = document.getElementById("submitLine");
-	// lineButtonClick.onclick = displayLineChart();
-	// if (lineButtonClick.addEventListener){
-	// 	lineButtonClick.addEventListener("click", displayLineChart(), false);
-	// 	console.log("line event");}
-	// else if (lineButtonClick.attachEvent)
-	// 	{console.log("line attach");
-	// 	lineButtonClick.attachEvent('onclick', displayLineChart());}
 
-	// var barButtonClick = document.getElementById("submitBar");
-	// barButtonClick.onclick = displayBarChart();
-	// if (barButtonClick.addEventListener)
-	// 	{console.log("bar event");
-	// 	barButtonClick.addEventListener("click", displayBarChart(), false);}
-	// else if (barButtonClick.attachEvent)
-	// 	{console.log("bar attach");
-	// 	barButtonClick.attachEvent('onclick', displayBarChart());}
-	var lineButtonClick = $("#submitLine");
-	lineButtonClick.on("click", displayLineChart());
-	var barButtonClick = $("#submitBarButton");
-	barButtonClick.on("click", displayBarChart());
-// 	var bns = $("#submitLine");
-// bns.on("click", function() {
-//     alert("you clicked line");
-// });
-// var bns = $("#submitBar");
-// bns.on("click", function() {
-// 	alert("you clicked bar");
-// });
-	// location = "visualize.html";
+	if(typeChart=='line'){
+		displayLineChart();
+	}
+	else if (typeChart=='bar'){
+		displayBarChart();
+	}
+	// else if (typeChart=='scatter'){
+	// 	displayScatterPlot();
+	// }
+
 	return false;
 }
 
 function displayLineChart(){
 	//loadData();
 	console.log(JSON.stringify(moodData));
-	var chartContext = document.getElementById("moodChartDiv").getContext("2d");
+	chartContext = document.getElementById("moodChartDiv").getContext("2d");
 	var data = {
 	labels: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"],
 	datasets: [
@@ -85,7 +76,7 @@ function displayLineChart(){
 function displayBarChart(){
 	//loadData();
 	console.log(JSON.stringify(moodData));
-	var chartContext = document.getElementById("moodChartDiv").getContext("2d");
+	chartContext = document.getElementById("moodChartDiv").getContext("2d");
 	var data = {
 	labels: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"],
 	datasets: [
@@ -114,3 +105,39 @@ function displayBarChart(){
 
 	window.moodChart = new Chart(chartContext).Bar(data,options);
 }
+
+// function displayScatterPlot(){
+// 	//loadData();
+// 	console.log(JSON.stringify(moodData));
+// 	chartContext = document.getElementById("moodChartDiv").getContext("2d");
+// 	var data = {
+// 	labels: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"],
+// 	datasets: [
+// 		{
+// 			fillColor: "rgba(75,192,192,0.2)",
+// 			borderColor: "rgba(75,192,192,1)",
+// 			pointBackgroundColor: "#fff",
+// 			pointRadius: 5,
+// 			data: moodData,
+// 			}
+// 		]
+// 	};
+// 	var options = {
+// 		scaleOverride : true,
+// 		scaleStartValue : 0,
+// 		scaleSteps : 10,
+// 		scaleStepWidth : 1,
+// 		tooltips: {
+// 			mode: "label"
+// 		}
+// 	};
+// 	if(window.moodChart != null){
+//     window.moodChart.destroy();
+// }
+//
+// 	window.moodChart = new Chart(chartContext,{
+// 		type: 'polarArea',
+// 		data: data,
+// 		options: options
+// 	});
+// }
